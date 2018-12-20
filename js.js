7cb9ec001701b8a2ff2da61261c9b5420cf46f5e
pabloborrajo1988 <45261360+pabloborrajo1988@users.noreply.github.com>
2018-12-20 13:38:50 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+/*
+Tu nombre y apellidos
+Tu edad
+Los días de la semana
+El valor de la gravedad
+Los 3 primeros números impares
+*/
+var nombre="Pablo";
+var apellidos="Borrajo Legeren";
+var edad=30;
+var dias=["lunes","martes","miercoles","jueves","vienes","sabado","domingo"];
+var gravedad=9.80665 ;
+var impares=[1,3,5];
